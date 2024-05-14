f73b6a601bb6a4dd04a36e6f4c08e81baec7ba4a
gaug1801 <gaug1801@gmail.com>
2024-05-14 12:14:17 -0500

    Practice with math functions. Circumference and Area of a cirle Volume of a sphere

--- index.php
+++ index.php
@@ -7,35 +7,29 @@
 </head>
 <body>
   <form action="index.php" method="post">
-    <label>x: </label>
-    <input type="text" name="x">
-    <label>y: </label>
-    <input type="text" name="y">
-    <label>z: </label>
-    <input type="text" name="z">
-    <input type="submit" value="total">
-  </form>
+    <label>radius: </label>
+    <input type="text" name="radius">
+    <input type="submit" value="calculate">
+  </form><br>
 </body>
 </html>
 
-
 <?php
-  $x = $_POST["x"];
-  $y = $_POST["y"];
-  $z = $_POST["z"];
-  $total = null;
+  $radius = $_POST["radius"];
+  $circumference = null;
+  $volume = null;
 
-  // $total = abs($x);
-  // $total = round($x);
-  // $total = floor($x);
-  // $total = ceil($x);
-  // $total = sqrt($x);
-  // $total = pow($x, $y);
-  // $total = max($x, $y, $z);
-  // $total = min($x, $y, $z);
-  // $total = pi();
-  // $total = rand(); // no parameter will choose a rand a little over  2 billion
-  $total = rand(1, 100); // a number from 1 to 100
-  echo $total;
+  $circumference = 2 * pi() * $radius;
+  $circumference = round($circumference, 2); //the second parameter determines how many decimal points to display
+
+  $area = pi() * pow($radius, 2);
+  $area = round($area, 2);
+
+  $volume = (4 / 3) * pi() * pow($radius, 3);
+  $volume = round($volume, 2);
+
+  echo"Circumference = {$circumference}cm<br>";
+  echo"Area = {$area}cm^2<br>";
+  echo"Volume = {$volume}cm^3<br>";
 
 ?>
