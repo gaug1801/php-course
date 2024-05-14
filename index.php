60d04d6d4bc29dd24f8d435f61651fd5535a9b2c
gaug1801 <gaug1801@gmail.com>
2024-05-14 11:26:33 -0500

    Arithmetic

--- index.php
+++ index.php
@@ -1,45 +1,22 @@
 <?php
-  // strings
-  $name = "Gabriel Augustin";
-  $food = "Pizza";
-  $email = "fake123@gmail.com";
+// $x = 10;
+// $y = 2;
+// $z = null;
 
-  // ints
-  $age = 24;
-  $users = 2;
-  $quantity = 5;
+// $z = $x / $y;
+// $z = $x + $y;
+// $z = $x - $y;
+// $z = $x * $y;
+// $z = $x ** $y;
+// $z = $x % $y;
+// echo"{$z}";
 
-  // floats
-  $gpa = 2.5;
-  $price = 5.29;
-  $tax_rate = 5.1;
+// $counter = 0;
+// $counter--;
+// $counter+= 3;
+// $counter++;
+// echo"{$counter}";
 
-  // boolean
-  $employed = true;
-  $online = false;
-  $for_sale = true;
-
-  // Nothing assigned
-  $total = null;
-
-  echo"You have ordered {$quantity} x {$food}s<br>";
-  $total = $quantity * $price;
-  echo"Your total is \${$total}";
-  
-
-  // echo "Hello {$name} <br>";
-  // echo "You like {$food} <br>";
-  // echo"Your email is {$email}<br>";
-
-  // echo "You are {$age} years old.<br>";
-  // echo"There are {$users} users online.<br>";
-  // echo"You would like to buy {$quantity} items.";
-
-  // echo"Your GPA is {$gpa}<br>";
-  // echo"Your pizza is \${$price}<br>";
-  // echo"The sales tax rate is {$tax_rate}%<br>";
-
-  // // If an output is false, nothing is displayed
-  // echo"Online Status: {$online}";
-  // echo"";
+$total = 1 + 2 - 3 * 4 / 5 ** 6;
+echo"{$total}";
 ?>
