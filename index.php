52bd4fed6dcdc594396f4c30b975231ab32c23d5
gaug1801 <gaug1801@gmail.com>
2024-05-14 10:48:43 -0500

    Upload Files

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  echo"I love Pizza <br>";
+  echo"It's really good";
+  //This is a comment
+  /* This 
+  is 
+  a multiline comment*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <br>
+  <button>Order Pizza</button>
+</body>
+</html>
