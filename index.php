fe82bfa9c96d7dd6243d7d3b1cdbbb87b1c7769a
gaug1801 <gaug1801@gmail.com>
2024-05-14 11:54:47 -0500

    More $_GET & $_POST practice

--- index.php
+++ index.php
@@ -6,40 +6,24 @@
   <title>Document</title>
 </head>
 <body>
-  <!-- method="get" will display the information being passed to php in the url!!! NOT SECURE -->
-   <form action="index.php" method="post">   <!-- Here we are sending the code back to the same file but for the php section to work with.-->
-  <label>username:</label><br>
-  <input type="text" name="username"><br>
-  <label>password:</label><br>
-  <input type="password" name="password"><br>
-  <button type="submit" value="Log in">Log in </button>
+  <form action="index.php" method="get">
+    <label>quantity: </label><br>
+    <input type="text" name="quantity"><br>
+    <input type="submit" value="total">
+
+
   </form>
 </body>
 </html>
 
 <?php
-  echo "{$_POST["username"]} <br>";
-  echo "{$_POST["password"]} <br>";
+// USING $_GET allows the user to edit the value in the url.
+  $item = "pizza";
+  $price = 5.99;
+  $quantity = $_GET["quantity"];
 
+  $total = $quantity * $price;
 
-// $_GET, $_POST = Special variable used to collect data from an HTML
-//                 form data is sent to the file in the action attribute
-//                 of <form>: <form action="some_file.php" method="get">
-
-// $_GET = Data is appended to the url
-//         NOT SECURE
-//         char limit
-//         Bookmark is possible with values
-//         GET requests can be cached
-//         Better for a search page
-
-// $_POST = Data is packaged inside the body of a HTTP request
-//          MORE SECURE
-//          No data limit
-//          Cannot bookmark
-//          GET requests are not cached
-//          Better for submitting credentials
-
-
-
+  echo"You have ordered {$quantity} x {$item}/s <br>";
+  echo"Your total is: \${$total}";
 ?>
