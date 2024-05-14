9efd6b242a4f31d54bc68ff5b3103faee42f97cb
gaug1801 <gaug1801@gmail.com>
2024-05-14 11:47:43 -0500

    $_GET & $_POST practice

--- index.php
+++ index.php
@@ -1,22 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- method="get" will display the information being passed to php in the url!!! NOT SECURE -->
+   <form action="index.php" method="post">   <!-- Here we are sending the code back to the same file but for the php section to work with.-->
+  <label>username:</label><br>
+  <input type="text" name="username"><br>
+  <label>password:</label><br>
+  <input type="password" name="password"><br>
+  <button type="submit" value="Log in">Log in </button>
+  </form>
+</body>
+</html>
+
 <?php
-// $x = 10;
-// $y = 2;
-// $z = null;
+  echo "{$_POST["username"]} <br>";
+  echo "{$_POST["password"]} <br>";
 
-// $z = $x / $y;
-// $z = $x + $y;
-// $z = $x - $y;
-// $z = $x * $y;
-// $z = $x ** $y;
-// $z = $x % $y;
-// echo"{$z}";
 
-// $counter = 0;
-// $counter--;
-// $counter+= 3;
-// $counter++;
-// echo"{$counter}";
+// $_GET, $_POST = Special variable used to collect data from an HTML
+//                 form data is sent to the file in the action attribute
+//                 of <form>: <form action="some_file.php" method="get">
 
-$total = 1 + 2 - 3 * 4 / 5 ** 6;
-echo"{$total}";
+// $_GET = Data is appended to the url
+//         NOT SECURE
+//         char limit
+//         Bookmark is possible with values
+//         GET requests can be cached
+//         Better for a search page
+
+// $_POST = Data is packaged inside the body of a HTTP request
+//          MORE SECURE
+//          No data limit
+//          Cannot bookmark
+//          GET requests are not cached
+//          Better for submitting credentials
+
+
+
 ?>
