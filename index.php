6a2afafe161e2e29b31b2cc8613b0071721b54e1
gaug1801 <gaug1801@gmail.com>
2024-05-16 12:05:48 -0500

    radio buttons

--- index.php
+++ index.php
@@ -7,61 +7,37 @@
 </head>
 <body>
   <form action="index.php" method="post">
-    <label>Enter a country:</label>
-    <input type="text" name="country">
-    <input type="submit">
+    <!-- By giving all three radios the same name attribute we can select only one at a time. -->
+    <input type="radio" name="credit_card" value="Visa"> Visa <br>
+    <input type="radio" name="credit_card" value="Mastercard"> Mastercard <br>
+    <input type="radio" name="credit_card" value="American Express"> American Express <br>
+    <input type="submit" name="confirm" value="Confirm">
   </form>
 </body>
 </html>
 
 <?php
-  //associative array = an array of key=>value pairs
-  //countries => capital
-  //id => username
-  //item => price
 
-  $capitals = array("USA"=>"Washington D.C.", 
-                    "Japan"=>"Kyoto", 
-                    "South Korea" => "Seoul", 
-                    "India"=> "New Delhi");
+  if (isset($_POST["confirm"])) { // checks that confirm has been clicked
 
-  
-  $capital = $capitals[$_POST["country"]]; //access value
+    $credit_card = null;
 
-  echo "The capital is " . $capital;
-
-
-
-  // $capitals["USA"] = "Las Vegas"; //changing a value
-  // $capitals["China"] = "Beijing"; // add new value
-  // array_pop($capitals); //remove last element
-  // array_shift($capitals);
-  // foreach($capitals as $key => $value) {
-  //   echo"{$key} = {$value} <br>";
-  // }
-
-  // $keys = array_keys($capitals); //get keys -- generates a new array.
-  // foreach($keys as $key) {
-  //   echo"{$key} <br>";
-  // }
-
-  // $values = array_values($capitals); //get values -- generates a new array.
-  // foreach($values as $value) {
-  //   echo"{$value} <br>";
-  // }
-
-  // $capitals = array_flip($capitals); // Flip keys and values around.
-  // foreach($capitals as $key => $value) {
-  //   echo"{$key} = {$value} <br>";
-  // }
-
-
-  // $capitals = array_reverse($capitals); // Order is now reversed.
-  // foreach($capitals as $key => $value) {
-  //   echo"{$key} = {$value} <br>";
-  // }
-
-  // echo count($capitals);
-
-
+    if (isset($_POST["credit_card"])) { // checks if credit card has been selected
+      $credit_card = $_POST["credit_card"];
+    }
+    switch($credit_card) {
+      case "Visa":
+        echo "You selected Visa.";
+        break;
+      case "Mastercard":
+        echo "You selected Mastercard.";
+        break;
+      case "American Express":
+        echo "You selected American Express.";
+        break;
+      default:
+        echo "Please make a selection.";
+        break;
+    }
+  }
 ?>
