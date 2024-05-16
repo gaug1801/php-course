a2373a4a7d59fe1e67a345465f755c10875d87d4
gaug1801 <gaug1801@gmail.com>
2024-05-16 11:25:28 -0500

    associative arrays and manipulation

--- index.php
+++ index.php
@@ -1,15 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <label>Enter a country:</label>
+    <input type="text" name="country">
+    <input type="submit">
+  </form>
+</body>
+</html>
+
 <?php
+  //associative array = an array of key=>value pairs
+  //countries => capital
+  //id => username
+  //item => price
 
-  $foods = array("apple", "orange", "banana", "coconut");
+  $capitals = array("USA"=>"Washington D.C.", 
+                    "Japan"=>"Kyoto", 
+                    "South Korea" => "Seoul", 
+                    "India"=> "New Delhi");
 
-  // $foods[0] = "pineapple";
-  // array_push($foods, "pineapple", "kiwi"); //add a new variable to the end.
-  // array_pop($foods);
-  // array_shift($foods); // removes the first variable
-  // $foods = array_reverse($foods); //returns a new array, so assign it to a new variable
-  // echo count($foods); //array count
+  
+  $capital = $capitals[$_POST["country"]]; //access value
 
-  foreach ($foods as $food) {
-    echo $food . "<br>";
-  }
+  echo "The capital is " . $capital;
+
+
+
+  // $capitals["USA"] = "Las Vegas"; //changing a value
+  // $capitals["China"] = "Beijing"; // add new value
+  // array_pop($capitals); //remove last element
+  // array_shift($capitals);
+  // foreach($capitals as $key => $value) {
+  //   echo"{$key} = {$value} <br>";
+  // }
+
+  // $keys = array_keys($capitals); //get keys -- generates a new array.
+  // foreach($keys as $key) {
+  //   echo"{$key} <br>";
+  // }
+
+  // $values = array_values($capitals); //get values -- generates a new array.
+  // foreach($values as $value) {
+  //   echo"{$value} <br>";
+  // }
+
+  // $capitals = array_flip($capitals); // Flip keys and values around.
+  // foreach($capitals as $key => $value) {
+  //   echo"{$key} = {$value} <br>";
+  // }
+
+
+  // $capitals = array_reverse($capitals); // Order is now reversed.
+  // foreach($capitals as $key => $value) {
+  //   echo"{$key} = {$value} <br>";
+  // }
+
+  // echo count($capitals);
+
+
 ?>
