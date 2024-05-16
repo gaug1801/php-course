16b7e4697d2f442a0506f8dad3da495d925df56f
gaug1801 <gaug1801@gmail.com>
2024-05-16 10:36:08 -0500

    for loops

--- index.php
+++ index.php
@@ -1,55 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <label>Enter a number to count to</label>
+    <input type="text" name="counter">
+    <input type="submit" value="start">
+  </form>
+</body>
+</html>
+
 <?php 
-  //swich = replacement to using many elif statements. less code
 
-  $date = "Monday";
+  $counter = $_POST["counter"];
 
-  switch($date) {
-    case "Monday":
-      echo"I hate mondays.";
-      break;
-    case "Tuesday":
-      echo"It is taco Tuesday.";
-      break;
-    case "Wednesday":
-      echo"The work week is half over.";
-      break;
-    case "Thursday":
-      echo"It's almost the weekend.";
-      break;
-    case "Friday":
-      echo"The weekend is here!";
-      break;
-    case "Saturday":
-      echo"It's time to party!";
-      break;
-    case "Sunday":
-      echo"Time to rest.";
-      break;
-    default:
-      echo"{$date} is not a date.";
-      break;
+  for ($i = $counter; $i > 0; $i--) {
+    echo $i . "<br>";
   }
 
-
-  // $grade = "Pizza";
-
-  // switch($grade){ 
-  //   case "A":
-  //     echo"You did great.";
-  //     break;
-  //   case "B":
-  //     echo"You did good.";
-  //     break;
-  //   case "C":
-  //     echo"You did okay.";
-  //     break;
-  //   case "D":
-  //     echo"You did poorly.";
-  //     break;
-  //   case "D":
-  //     echo"You failed.";
-  //     break;
-  //   default:
-  //     echo"{$grade} is not valid.";
+  // for ($i = 1; $i <= $counter; $i++) {
+  //   echo $i . "<br>";
   // }
 ?>
