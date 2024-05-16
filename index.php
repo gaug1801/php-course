7f89b85b6d5aa99ae1431ce47f37b0a35e33b80b
gaug1801 <gaug1801@gmail.com>
2024-05-16 10:29:18 -0500

    Swich cases

--- index.php
+++ index.php
@@ -1,41 +1,55 @@
 <?php 
-  $child = false;
-  $senior = true;
-  $ticket = null;
+  //swich = replacement to using many elif statements. less code
 
-  if ($child || $senior) {
-    $ticket = 10;
+  $date = "Monday";
+
+  switch($date) {
+    case "Monday":
+      echo"I hate mondays.";
+      break;
+    case "Tuesday":
+      echo"It is taco Tuesday.";
+      break;
+    case "Wednesday":
+      echo"The work week is half over.";
+      break;
+    case "Thursday":
+      echo"It's almost the weekend.";
+      break;
+    case "Friday":
+      echo"The weekend is here!";
+      break;
+    case "Saturday":
+      echo"It's time to party!";
+      break;
+    case "Sunday":
+      echo"Time to rest.";
+      break;
+    default:
+      echo"{$date} is not a date.";
+      break;
   }
-  else {
-    $ticket = 15;
-  }
 
-  echo "The ticket price is \${$ticket}";
 
-  // $age = 12;
-  // $citizen = true;
-  // if (!($age >= 18) || (!$citizen)) {
-  //   echo"You cannot vote.";
-  // }
-  // else {
-  //   echo"You can vote.";
-  // }
+  // $grade = "Pizza";
 
-  // $temp = 15;
-  // $cloud = true;
-
-  // if($temp < 0 || $temp > 30) {
-  //   echo"The weather is bad.<br>";
+  // switch($grade){ 
+  //   case "A":
+  //     echo"You did great.";
+  //     break;
+  //   case "B":
+  //     echo"You did good.";
+  //     break;
+  //   case "C":
+  //     echo"You did okay.";
+  //     break;
+  //   case "D":
+  //     echo"You did poorly.";
+  //     break;
+  //   case "D":
+  //     echo"You failed.";
+  //     break;
+  //   default:
+  //     echo"{$grade} is not valid.";
   // }
-  // else {
-  //   echo"The weather is good.<br>";
-  // }
-
-  // if (!$cloudy) {
-  //   echo"It's sunny.";
-  // }
-  // else {
-  //   echo"It's cloudy.";
-  // }
-
 ?>
