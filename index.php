1c026d346f2b7849a2fb07b2d931a189d50a9cb3
gaug1801 <gaug1801@gmail.com>
2024-06-06 11:53:48 -0500

    cookies

--- index.php
+++ index.php
@@ -1,25 +1,28 @@
 <?php 
-  // include() = Copies of a file (php/html/text)
-  //             and includes it in your php file.
-  //             Sections of your website become reusable
-  //             Changes only need to be made in one place.
 
-  include("header.html");
-?>
+  // cookie = Info about a user stored in a user's web-browser
+  //          why you receive targeted ads, browsing preferences, and
+  //          other non-sensitive data.
+  //setcookie(name, value, expiration time, path)
+  setcookie("fav_food", "pizza", time() - 0, "/"); //stored as an associative array
+  setcookie("fav_drink", "coffee", time() + (86400 * 3 ), "/");
+  setcookie("fav_dessert", "ice cream", time() + (86400 * 4 ), "/");
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  This is the Home Page<br>
-  Stuff about your home page can go here.
-</body>
-</html>
+  // to delete a cookie, set the time to time() - 0
+  // setcookie("fav_food", "pizza", time() - 0, "/");
+  // setcookie("fav_drink", "coffee", time() - 0, "/");
+  // setcookie("fav_dessert", "ice cream", time() - 0, "/");
 
-<?php 
-  include("footer.html");
+  // Print all value key pairs
+  foreach($_COOKIE as $key => $value) {
+    echo"{$key} = {$value} <br>";
+  }
+
+  if (isset($_COOKIE["fav_food"])) {
+    echo"BUY SOME {$_COOKIE["fav_food"]} !!!";
+  }
+  else {
+    echo"I don't know your favorite food.";
+  }
+
 ?>
