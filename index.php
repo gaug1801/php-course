be7867b6d52b3f63a90c6a6f2900ff1d3c34a574
gaug1801 <gaug1801@gmail.com>
2024-06-06 11:42:42 -0500

    include() function

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+  // include() = Copies of a file (php/html/text)
+  //             and includes it in your php file.
+  //             Sections of your website become reusable
+  //             Changes only need to be made in one place.
+
+  include("header.html");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,53 +15,11 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="index.php" method="post">
-    username: <br>
-    <input type="text" name="username">
-    <br>
-    age: <br>
-    <input type="text" name="age">
-    <br>
-    email:
-    <br> 
-    <input type="text" name="email">
-    <input type="submit" name="submit">
-
-  </form>
+  This is the Home Page<br>
+  Stuff about your home page can go here.
 </body>
 </html>
- <?php 
-    if (isset($_POST["submit"])) {
-      /*    VERIFY AN INPUT - returns an empty string if it doesn't pass the verif. 
-      // $age = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT); // if the input is NAN return empty.
 
-      // if (empty($age)) {
-      //   echo "that number wasn't valid";
-      // }
-      // else {
-      //   echo "You are {$age} years old.";
-      // }
-
-      // $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);
-      // if (empty($email)) {
-      //   echo "that email wasn't valid";
-      // }
-      // else {
-      //   echo "Your email is: {$email}";
-      // }
-      */
-      
-      /*    SANITIZE AN INPUT - strip certain chars from the input
-      $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);//$_POST["username"];//Instead of using the $_POST array, use filter_input()
-      //filter_input() parameters: INPUT_POST (or GET), name of the input (username), what you want to filter out.
-      //In this case we are filtering all speical chars from the input, so we use FILTER_SANITIZE_SPECIAL_CHARS.
-
-      $age = filter_input(INPUT_POST, "age", FILTER_SANITIZE_NUMBER_INT);
-
-      $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-      echo "Your email is: {$email}";
-      */
-    }
-
-
- ?>
+<?php 
+  include("footer.html");
+?>
---
+++ locations.php
@@ -0,0 +1,19 @@
+<?php
+  include("header.html")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+This is the Locations Page<br>
+  Stuff about your locations page can go here.
+</body>
+</html>
+
+<?php
+  include("footer.html")
+?>
